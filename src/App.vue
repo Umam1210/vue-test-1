4c4cfe7314881ec195b125d005749f995f71ddc2
Umam1210 <khairulumam950@gmail.com>
2022-12-22 09:40:11 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-warning" style="height: 100vh;">
+    <div id="app" class="d-flex justify-content-center">
+      <div>
+        <input type="text" placeholder="Ketik nama anda..." ref="inputField" style="width: 50vh;"
+          class="rounded mt-4 py-1" />
+      </div>
+    </div>
+    <div class="d-flex justify-content-center mt-3">
+      <button style="" type="button" class="bg-success border border-white py-1 px-2 text-white rounded" @click="handleShout" :disabled="message === '' ">Buat Profile Picture!</button>
+    </div>
+    <div class="d-flex justify-content-center" style="height: 50vh;">
+      <div class="d-flex justify-content-center mt-5" v-if="shout === shout">
+        <div style="background-color:aqua ; border: 21;" class="rounded-circle">
+          <img :src="shout" fluid />
+        </div>
+      </div>
+      <div v-else></div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      shout: '',
+    };
+  },
+  methods: {
+    handleShout() {
+      const message = this.$refs.inputField.value;
+      this.shout = `https://robohash.org/${message}`;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
